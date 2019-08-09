be7b73de753defc8f30e03f827619097530b27b1
Saúl Ibarra Corretgé <s@saghul.net>
2019-08-09 09:59:17 +0200

    repl: don't use strip mode
    This way we retain line numbers in tracebaks.

--- src/repl.js
+++ src/repl.js
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-"use strip";
+"use strict";
 
 import * as std from "std";
 import * as uv from "uv";
