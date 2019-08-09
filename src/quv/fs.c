40110bb42c828ddb37ab3ae9a2d48995507809cb
Saúl Ibarra Corretgé <s@saghul.net>
2019-08-10 00:04:37 +0200

    fs: add stat / lstat / fstst

--- src/quv/fs.c
+++ src/quv/fs.c
@@ -68,6 +68,31 @@ typedef struct {
     } rw;
 } JSUVFsReq;
 
+static JSValue js__stat2obj(JSContext *ctx, uv_stat_t *st) {
+    JSValue obj = JS_NewObjectProto(ctx, JS_NULL);
+#define SET_UINT64_FIELD(x) JS_SetPropertyStr(ctx, obj, stringify(x), JS_NewBigUint64(ctx, st->x))
+    SET_UINT64_FIELD(st_dev);
+    SET_UINT64_FIELD(st_mode);
+    SET_UINT64_FIELD(st_nlink);
+    SET_UINT64_FIELD(st_uid);
+    SET_UINT64_FIELD(st_gid);
+    SET_UINT64_FIELD(st_rdev);
+    SET_UINT64_FIELD(st_ino);
+    SET_UINT64_FIELD(st_size);
+    SET_UINT64_FIELD(st_blksize);
+    SET_UINT64_FIELD(st_blocks);
+    SET_UINT64_FIELD(st_flags);
+    SET_UINT64_FIELD(st_gen);
+#undef SET_UINT64_FIELD
+#define SET_TIMESPEC_FIELD(x) JS_SetPropertyStr(ctx, obj, stringify(x), JS_NewFloat64(ctx, st->x.tv_sec + 1e-9*st->x.tv_nsec))
+    SET_TIMESPEC_FIELD(st_atim);
+    SET_TIMESPEC_FIELD(st_mtim);
+    SET_TIMESPEC_FIELD(st_ctim);
+    SET_TIMESPEC_FIELD(st_birthtim);
+#undef SET_TIMESPEC_FIELD
+    return obj;
+}
+
 static JSValue js_new_uv_file(JSContext *ctx, uv_file fd, const char *path) {
     JSUVFile *f;
     JSValue obj;
@@ -151,6 +176,12 @@ static void uv__fs_req_cb(uv_fs_t* req) {
     case UV_FS_READ:
     case UV_FS_WRITE:
         arg = JS_NewInt32(ctx, fr->req.result);
+        break;
+
+    case UV_FS_STAT:
+    case UV_FS_LSTAT:
+    case UV_FS_FSTAT:
+        arg = js__stat2obj(ctx, &fr->req.statbuf);
         break;
 
     default:
@@ -263,6 +294,29 @@ static JSValue js_uv_file_close(JSContext *ctx, JSValueConst this_val, int argc,
     return fr->result.promise;
 }
 
+static JSValue js_uv_file_stat(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
+    JSUVFile *f = js_uv_file_get(ctx, this_val);
+    if (!f)
+        return JS_EXCEPTION;
+
+    uv_loop_t *loop = js_uv_get_loop(ctx);
+    if (!loop)
+        return JS_ThrowInternalError(ctx, "couldn't find libuv loop");
+
+    JSUVFsReq *fr = js_malloc(ctx, sizeof(*fr));
+    if (!fr)
+        return JS_EXCEPTION;
+
+    int r = uv_fs_fstat(loop, &fr->req, f->fd, uv__fs_req_cb);
+    if (r != 0) {
+        js_free(ctx, fr);
+        return js_uv_throw_errno(ctx, r);
+    }
+
+    js_uv_fsreq_init(ctx, fr, this_val);
+    return fr->result.promise;
+}
+
 static JSValue js_uv_file_fileno(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
     JSUVFile *f = js_uv_file_get(ctx, this_val);
     if (!f)
@@ -348,16 +402,46 @@ static JSValue js_uv_fs_open(JSContext *ctx, JSValueConst this_val, int argc, JS
     return fr->result.promise;
 }
 
+static JSValue js_uv_fs_stat(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv, int magic) {
+    uv_loop_t *loop = js_uv_get_loop(ctx);
+    if (!loop)
+        return JS_ThrowInternalError(ctx, "couldn't find libuv loop");
+
+    const char *path = JS_ToCString(ctx, argv[0]);
+    if (!path)
+        return JS_EXCEPTION;
+
+    JSUVFsReq *fr = js_malloc(ctx, sizeof(*fr));
+    if (!fr)
+        return JS_EXCEPTION;
+
+    int r;
+    if (magic)
+        r = uv_fs_lstat(loop, &fr->req, path, uv__fs_req_cb);
+    else
+        r = uv_fs_stat(loop, &fr->req, path, uv__fs_req_cb);
+    if (r != 0) {
+        js_free(ctx, fr);
+        return js_uv_throw_errno(ctx, r);
+    }
+
+    js_uv_fsreq_init(ctx, fr, JS_UNDEFINED);
+    return fr->result.promise;
+}
+
 static const JSCFunctionListEntry js_uv_file_proto_funcs[] = {
     JS_CFUNC_MAGIC_DEF("read", 4, js_uv_file_rw, 0 ),
     JS_CFUNC_MAGIC_DEF("write", 4, js_uv_file_rw, 1 ),
     JS_CFUNC_DEF("close", 0, js_uv_file_close ),
     JS_CFUNC_DEF("fileno", 0, js_uv_file_fileno ),
+    JS_CFUNC_DEF("stat", 0, js_uv_file_stat ),
     JS_CGETSET_DEF("path", js_uv_file_path_get, NULL ),
 };
 
 static const JSCFunctionListEntry js_uv_fs_funcs[] = {
     JS_CFUNC_DEF("open", 3, js_uv_fs_open ),
+    JS_CFUNC_MAGIC_DEF("stat", 1, js_uv_fs_stat, 0 ),
+    JS_CFUNC_MAGIC_DEF("lstat", 1, js_uv_fs_stat, 1 ),
 };
 
 void js_uv_mod_fs_init(JSContext *ctx, JSModuleDef *m) {
