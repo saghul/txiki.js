4d6330259c947340964028f305a78c80bde4a2f3
Saúl Ibarra Corretgé <s@saghul.net>
2019-08-14 21:42:41 +0200

    timers: cleanup some code

--- src/quv/timers.c
+++ src/quv/timers.c
@@ -65,22 +65,20 @@ static JSClassID quv_timer_class_id;
 static void quv_timer_finalizer(JSRuntime *rt, JSValue val)
 {
     JSUVTimer *th = JS_GetOpaque(val, quv_timer_class_id);
-    if (th) {
+    if (th)
         uv_close((uv_handle_t*)&th->handle, uv__timer_close);
-    }
 }
 
 static void quv_timer_mark(JSRuntime *rt, JSValueConst val,
                              JS_MarkFunc *mark_func)
 {
     JSUVTimer *th = JS_GetOpaque(val, quv_timer_class_id);
-    if (th) {
+    if (th)
         JS_MarkValue(rt, th->func, mark_func);
-    }
 }
 
 static JSClassDef quv_timer_class = {
-    "UVTimer",
+    "Timer",
     .finalizer = quv_timer_finalizer,
     .gc_mark = quv_timer_mark,
 }; 
@@ -92,33 +90,36 @@ static JSValue quv_setTimeout(JSContext *ctx, JSValueConst this_val,
     JSValueConst func;
     JSUVTimer *th;
     JSValue obj;
-    quv_state_t *quv_state;
 
-    quv_state = JS_GetContextOpaque(ctx);
-    if (!quv_state) {
-        return JS_ThrowInternalError(ctx, "couldn't find uv state");
-    }
+    uv_loop_t *loop = quv_get_loop(ctx);
+    if (!loop)
+        return JS_ThrowInternalError(ctx, "couldn't find libuv loop");
 
     func = argv[0];
     if (!JS_IsFunction(ctx, func))
         return JS_ThrowTypeError(ctx, "not a function");
+
     if (JS_ToInt64(ctx, &delay, argv[1]))
         return JS_EXCEPTION;
+
     obj = JS_NewObjectClass(ctx, quv_timer_class_id);
     if (JS_IsException(obj))
         return obj;
+
     th = js_mallocz(ctx, sizeof(*th));
     if (!th) {
         JS_FreeValue(ctx, obj);
         return JS_EXCEPTION;
     }
+
     th->ctx = ctx;
-    uv_timer_init(&quv_state->uvloop, &th->handle);
+    uv_timer_init(loop, &th->handle);
     th->handle.data = th;
     uv_timer_start(&th->handle, uv__timer_cb, delay, magic ? delay : 0 /* repeat */);
     th->interval = magic;
     th->func = JS_DupValue(ctx, func);
     th->obj = JS_DupValue(ctx, obj);  // incref
+
     JS_SetOpaque(obj, th);
     return obj;
 }
@@ -129,17 +130,15 @@ static JSValue quv_clearTimeout(JSContext *ctx, JSValueConst this_val,
     JSUVTimer *th = JS_GetOpaque2(ctx, argv[0], quv_timer_class_id);
     if (!th)
         return JS_EXCEPTION;
+
     uv_timer_stop(&th->handle);
     JSValue func = th->func;
-    if (!JS_IsUndefined(func)) {
-        th->func = JS_UNDEFINED;
-        JS_FreeValue(ctx, func);
-    }
+    th->func = JS_UNDEFINED;
+    JS_FreeValue(ctx, func);
     JSValue obj = th->obj;
-    if (!JS_IsUndefined(obj)) {
-        th->obj = JS_UNDEFINED;
-        JS_FreeValue(ctx, obj);  // decref
-    }
+    th->obj = JS_UNDEFINED;
+    JS_FreeValue(ctx, obj);  // decref
+
     return JS_UNDEFINED;
 }
 
