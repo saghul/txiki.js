a7542919323b78dc25bf6a1c69747408858b0bd2
Saúl Ibarra Corretgé <s@saghul.net>
2019-08-29 21:47:10 +0200

    core: add ability to import directly from HTTP(S) endpoints
    Use libcurl, if available at build time.

---
+++ examples/import_http.js
@@ -0,0 +1,7 @@
+
+import {
+    greet
+} from 'https://gist.githubusercontent.com/saghul/bbf3e585a567f1bc1e60181c45a1f890/raw/0ce7878553bc65f760b04384375c4624a702fa66/greeter.js';
+
+greet('saghul');
+
