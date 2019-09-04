0a78afb897df8da9b984ec6308fa7a020a43e20a
Saúl Ibarra Corretgé <s@saghul.net>
2019-09-04 22:34:11 +0200

    core: add version information

---
+++ tests/test-version.js
@@ -0,0 +1,11 @@
+import { test } from './t.js';
+
+test('version', t => {
+    t.ok(quv.version, 'quv.version is defined');
+});
+
+test('versions', t => {
+    t.ok(quv.versions, 'quv.versions is defined');
+    t.ok(quv.versions.quv, 'quv.versions.quv is defined');
+    t.ok(quv.versions.uv, 'quv.versions.uv is defined');
+});
