bb6d4ac4eb0f37b5f4174247945455295b0acf75
Saúl Ibarra Corretgé <s@saghul.net>
2019-09-04 23:18:48 +0200

    curl: add ability to bundle curl

--- tests/run.js
+++ tests/run.js
@@ -1,6 +1,7 @@
 import { run } from './t.js';
 
 import './test-args.js';
+import './test-import-http.js';
 import './test-noop.js';
 import './test-performance.js';
 import './test-version.js';
---
+++ tests/test-import-http.js
@@ -0,0 +1,8 @@
+import { test } from './t.js';
+import 'https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.js';
+
+test('import from HTTP works', t => {
+    const words = ['sky', 'wood', 'forest', 'falcon', 'pear', 'ocean', 'universe'];
+    t.eq(_.first(words), 'sky', '_.first works');
+    t.eq(_.last(words), 'universe', '_.last works');
+});
