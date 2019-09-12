377834f14ca5fd34652ef8bf9bbd4fc07c0764e8
Saúl Ibarra Corretgé <s@saghul.net>
2019-09-12 22:41:39 +0200

    test: add basic XHR and fetch tests

--- tests/run.js
+++ tests/run.js
@@ -1,10 +1,12 @@
 import { run } from './t.js';
 
 import './test-args.js';
+import './test-fetch.js';
 import './test-global-window.js';
 import './test-import-http.js';
 import './test-noop.js';
 import './test-performance.js';
 import './test-version.js';
+import './test-xhr.js';
 
 run();
