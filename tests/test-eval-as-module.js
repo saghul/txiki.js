306dae329f820842c7164ab47adf51cfc54a5c60
Saúl Ibarra Corretgé <s@saghul.net>
2022-02-04 14:04:58 +0100

    cli: always eval script as a module
    This also removes the now useless strict-module-detection flag.

---
+++ tests/test-eval-as-module.js
@@ -0,0 +1,15 @@
+import assert from './assert.js';
+import { path } from '@tjs/std';
+
+
+(async () => {
+    const args = [
+        tjs.exepath(),
+        path.join(import.meta.dirname, 'helpers', 'log-import-meta.js')
+    ];
+    const proc = tjs.spawn(args);
+    const status = await proc.wait();
+    // If the file is evaluated as a global script instead of a module, it will give an error
+    // because import.meta cannot be used in that case.
+    assert.ok(status.exit_status === 0 && status.term_signal === 0, 'succeeded')
+})();
