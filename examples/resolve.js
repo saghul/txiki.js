44623d5d25069f46ff0ee125a579c58569f31d1b
Saúl Ibarra Corretgé <s@saghul.net>
2022-02-09 11:02:16 +0100

    examples: update resolver example

--- examples/resolve.js
+++ examples/resolve.js
@@ -2,7 +2,7 @@
 //
 
 (async function() {
-    const res = await tjs.getaddrinfo(tjs.args[2]);
+    const res = await tjs.getaddrinfo(tjs.args[2], tjs.args[3], { socktype: tjs.SOCK_STREAM, protocol: tjs.IPPROTO_TCP });
     console.log(JSON.stringify(res, undefined, 2));
 })().catch(e => {
     console.log(e);
