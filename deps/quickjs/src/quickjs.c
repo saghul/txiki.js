b1d7c87c66507be5f3226d2e40ef1a6b416f872b
Saúl Ibarra Corretgé <s@saghul.net>
2022-02-15 10:30:27 +0100

    oquickjs: fix build in MinGW

--- deps/quickjs/src/quickjs.c
+++ deps/quickjs/src/quickjs.c
@@ -69,7 +69,7 @@
 
 /* define to include Atomics.* operations which depend on the OS
    threads */
-#if !defined(EMSCRIPTEN)
+#if !defined(EMSCRIPTEN) && !defined(_WIN32)
 #define CONFIG_ATOMICS
 #endif
 
