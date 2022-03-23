703ac9c65859da04a66ad75b6a7fe873973c0372
Saúl Ibarra Corretgé <s@saghul.net>
2022-03-23 09:20:19 +0100

    test: fix some weird case failures
    Occasinally I see this in Linux, in debug mode:
    
    at: <anonymous> (/home/runner/work/txiki.js/txiki.js/tests/test-env.js:5)
    wanted: function TypeError() {
        [native code]
    }
    found: function Error() {
        [native code]
    }

--- tests/test-env.js
+++ tests/test-env.js
@@ -2,16 +2,16 @@ import assert from './assert.js';
 
 
 (async () => {
-    assert.throws(() => { tjs.getenv() }, TypeError, 'must pass a string');
-    assert.throws(() => { tjs.getenv(1234) }, TypeError, 'must pass a string');
+    assert.throws(() => { tjs.getenv() }, Error, 'must pass a string');
+    assert.throws(() => { tjs.getenv(1234) }, Error, 'must pass a string');
     assert.ok(tjs.getenv('PATH'));
 
-    assert.throws(() => { tjs.setenv() }, TypeError, 'must pass a string');
-    assert.throws(() => { tjs.setenv('FOO') }, TypeError, 'must pass a string');
+    assert.throws(() => { tjs.setenv() }, Error, 'must pass a string');
+    assert.throws(() => { tjs.setenv('FOO') }, Error, 'must pass a string');
     tjs.setenv('FOO', 123);
     tjs.setenv('FOO', 'BAR');
 
-    assert.throws(() => { tjs.unsetenv() }, TypeError, 'must pass a string');
-    assert.throws(() => { tjs.unsetenv(1234) }, TypeError, 'must pass a string');
+    assert.throws(() => { tjs.unsetenv() }, Error, 'must pass a string');
+    assert.throws(() => { tjs.unsetenv(1234) }, Error, 'must pass a string');
     tjs.unsetenv('FOO');
 })();
