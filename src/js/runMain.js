f3c84458202674db2531900b6e973f25686631a8
Saúl Ibarra Corretgé <s@saghul.net>
2022-11-29 22:24:34 +0100

    core: fix crash when running a file
    Now that the startup logic is in JS, we JS_Eval that file as part of
    TJS_Run.
    
    That file, in turn, may evaluate the given file as a module, which
    results on abother JS_Eval before we are done with the first.
    
    This seems to corrupt the vm state.
    
    The solution here was to queue a microtask, thus breaking away from the
    original problem. A hack to be sure...

--- src/js/runMain.js
+++ src/js/runMain.js
@@ -2,39 +2,34 @@
 
 import { getopts } from '@tjs/std';
 
-(() => {
-    const {
-        evalFile,
-        evalScript,
-        evalStdin,
-        isStdinTty,
-        runRepl
-    } = tjs[Symbol.for('tjs.internal')];
-    const options = getopts(tjs.args.slice(1), {
-        alias: {
-            eval: 'e',
-            help: 'h',
-            version: 'v'
-        },
-        boolean: [ 'h', 'v' ],
-        string: [ 'e' ]
-    });
+const {
+    evalFile,
+    evalScript,
+    evalStdin,
+    isStdinTty,
+    runRepl
+} = tjs[Symbol.for('tjs.internal')];
+const options = getopts(tjs.args.slice(1), {
+    alias: {
+        eval: 'e',
+        help: 'h',
+        version: 'v'
+    },
+    boolean: [ 'h', 'v' ],
+    string: [ 'e' ]
+});
+const [ filename ] = options['_'];
 
-    // TODO: move all options here.
+// TODO: move all options here.
 
-    if (options.eval) {
-        return evalScript(options.eval);
-    }
-
-    const [ filename ] = options['_'];
-
-    if (filename) {
-        return evalFile(filename);
-    }
-
-    if (isStdinTty()) {
-        return runRepl();
-    }
-
-    return evalStdin();
-})();
+if (options.eval) {
+    evalScript(options.eval);
+} else if (filename) {
+    // XXX: This looks weird. This file is being JS_Eval'd when we call `evalFile`,
+    // which does another JS_Eval, and something get's messed up :-(
+    globalThis.queueMicrotask(() => evalFile(filename));
+} else if (isStdinTty()) {
+    runRepl();
+} else {
+    evalStdin();
+}
