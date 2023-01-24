84d899ef9e69fa107a4c07f2ac3b6c4bdf4e2e37
Saúl Ibarra Corretgé <s@saghul.net>
2023-01-24 09:38:03 +0100

    wasm: fix error when parsing an empty file

--- tests/test-wasm-wasi.js
+++ tests/test-wasm-wasi.js
@@ -2,7 +2,7 @@ import assert from 'tjs:assert';
 import path from 'tjs:path';
 
 
-(async () => {
+async function testWasi() {
     const args = [
         tjs.exepath,
         'run',
@@ -10,8 +10,8 @@ import path from 'tjs:path';
     ];
     const proc = tjs.spawn(args, { stdout: 'pipe' });
     const status = await proc.wait();
-    assert.eq(status.exit_status, 0, 'WASI ran succesfully');
-    assert.eq(status.term_signal, null, 'WASI ran succesfully 2');
+    assert.eq(status.exit_status, 0, 'WASI ran successfully');
+    assert.eq(status.term_signal, null, 'WASI ran successfully 2');
     const buf = new Uint8Array(4096);
     const nread = await proc.stdout.read(buf);
     assert.ok(nread > 0, 'stdout was read');
@@ -20,4 +20,40 @@ import path from 'tjs:path';
     assert.ok(dataStr.match(/Constructor OK/), 'data matches 2');
     assert.ok(dataStr.match(/Hello printf!/), 'data matches 3');
     assert.ok(dataStr.match(/fib\(20\)/), 'data matches 4');
-})();
+}
+
+async function testEmptyWasm() {
+    const args = [
+        tjs.exepath,
+        'run',
+        path.join(import.meta.dirname, 'wasi', 'empty.wasm')
+    ];
+    const proc = tjs.spawn(args, { stdout: 'pipe' });
+    const status = await proc.wait();
+    assert.eq(status.exit_status, 1, 'WASI failed to run');
+    const buf = new Uint8Array(4096);
+    const nread = await proc.stdout.read(buf);
+    assert.ok(nread > 0, 'stdout was read');
+    const dataStr = new TextDecoder().decode(buf.subarray(0, nread));
+    assert.ok(dataStr.match(/TypeError: invalid buffer/));
+}
+
+async function testBadWasm() {
+    const args = [
+        tjs.exepath,
+        'run',
+        path.join(import.meta.dirname, 'wasi', 'bad.wasm')
+    ];
+    const proc = tjs.spawn(args, { stdout: 'pipe' });
+    const status = await proc.wait();
+    assert.eq(status.exit_status, 1, 'WASI failed to run');
+    const buf = new Uint8Array(4096);
+    const nread = await proc.stdout.read(buf);
+    assert.ok(nread > 0, 'stdout was read');
+    const dataStr = new TextDecoder().decode(buf.subarray(0, nread));
+    assert.ok(dataStr.match(/CompileError: underrun while parsing Wasm binary/));
+}
+
+testWasi();
+testEmptyWasm();
+testBadWasm();
