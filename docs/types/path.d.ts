88dc4a76128da6f725bd82a769aaba076112fd1c
Saúl Ibarra Corretgé <s@saghul.net>
2023-01-24 13:33:13 +0100

    docs: update documentation

---
+++ docs/types/path.d.ts
@@ -0,0 +1,37 @@
+/**
+ * Utilities for working with paths.
+ * This module is an adaptation of the [NodeJS path module](https://nodejs.org/docs/latest-v18.x/api/path.html).
+ *
+ * @module tjs:path
+ */
+
+export interface IPathObject {
+    dir: string;
+    root: string;
+    base: string;
+    name: string;
+    ext: string;
+}
+
+export interface IPath {
+    readonly delimiter: string;
+    readonly sep: string;
+
+    basename(path: string): string;
+    dirname(path: string): string;
+    extname(path: string): string;
+    format(pathObj: IPathObject): string;
+    isAbsolute(path: string): boolean;
+    join(paths: string[]): string;
+    normalize(path: string): string;
+    parse(path: string): IPathObject;
+    relative(from: string, to: string): string;
+    resolve(paths: string[]): string;
+    toNamespacedPath(path: string): string;
+}
+
+export const posix: IPath;
+export const win32: IPath;
+
+declare const path: IPath;
+export default path;
