70ecd524e19481df442264dbc4bc20313f4b956f
Saúl Ibarra Corretgé <s@saghul.net>
2023-01-31 13:49:48 +0100

    test: add test for "bad" JS

---
+++ tests/test-badjs.js
@@ -0,0 +1,18 @@
+import assert from 'tjs:assert';
+import path from 'tjs:path';
+
+const NUM_TRIES = 10;
+
+(async () => {
+    for (let i = 0; i < NUM_TRIES; i++) {
+        const args = [
+            tjs.exepath,
+            'run',
+            path.join(import.meta.dirname, 'helpers', 'bad.js')
+        ];
+        const proc = tjs.spawn(args);
+        const status = await proc.wait();
+
+        assert.ok(status.exit_status === 1 && status.term_signal === null, 'succeeded')
+    }
+})();
