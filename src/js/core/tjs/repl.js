ebb1175dd8200c1792fd62cb8dfaf3b6480ab833
Saúl Ibarra Corretgé <s@saghul.net>
2023-02-02 13:54:15 +0100

    repl: add initial support for await

--- src/js/core/tjs/repl.js
+++ src/js/core/tjs/repl.js
@@ -1130,12 +1130,33 @@ function _run(g) {
                      '\\q      exit\n');
     }
 
+    function preprocessEval(input) {
+        const awaitMatcher = /^(?:\s*(?:(?:let|var|const)\s)?\s*([^=]+)=\s*|^\s*)(await\s[\s\S]*)/;
+        const asyncWrapper = (code, binder) => {
+            let assign = binder ? `global.${binder} = ` : '';
+            return `(function(){ async function _wrap() { return ${assign}${code} } return _wrap();})()`;
+        };
+      
+        // match & transform
+        const match = input.match(awaitMatcher);
+        if (match) {
+            input = `${asyncWrapper(match[2], match[1])}`;
+        }
+        return input;
+      }
+
+    function doEval(input) {
+        const code = preprocessEval(input);
+
+        return evalScript(code);
+    }
+
     function eval_and_print(expr) {
         var result;
 
         try {
             /* eval as a script */
-            result = evalScript(expr);
+            result = doEval(expr);
             stdout_write(colors[styles.result]);
             print(result);
             stdout_write('\n');
