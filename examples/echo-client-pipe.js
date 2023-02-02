29db0a1265439e673aa743e3b4ad6e2e0d1ca41d
Saúl Ibarra Corretgé <s@saghul.net>
2023-02-02 13:38:14 +0100

    examples: use TLA

--- examples/echo-client-pipe.js
+++ examples/echo-client-pipe.js
@@ -2,19 +2,17 @@
 //
 
 
-(async () => {
-    const p = await tjs.connect('pipe', tjs.args[2] || '/tmp/fooPipe');
-    
-    console.log(`Connected to ${p.remoteAddress}`);
+const p = await tjs.connect('pipe', tjs.args[2] || '/tmp/fooPipe');
 
-    const buf = new Uint8Array(4096);
-    while (true) {
-        const nread = await p.read(buf);
-        if (nread === null) {
-            console.log('connection closed!');
-            break;
-        }
-        //console.log(`Received: ${new TextDecoder().decode(data)}`);
-        p.write(buf.slice(0, nread));
+console.log(`Connected to ${p.remoteAddress}`);
+
+const buf = new Uint8Array(4096);
+while (true) {
+    const nread = await p.read(buf);
+    if (nread === null) {
+        console.log('connection closed!');
+        break;
     }
-})();
+    //console.log(`Received: ${new TextDecoder().decode(data)}`);
+    p.write(buf.slice(0, nread));
+}
--- examples/process.js
+++ examples/process.js
@@ -5,54 +5,48 @@ function logStatus(s) {
 
 const exe = tjs.exepath;
 
-(async () => {
-    let args, status, proc, input, data, encoder = new TextEncoder(), decoder = new TextDecoder();
+let args, status, proc, input, data, encoder = new TextEncoder(), decoder = new TextDecoder();
 
-    args = [exe, '-e', 'console.log(1+1)'];
-    proc = tjs.spawn(args);
-    console.log(`proc PID: ${proc.pid}`);
-    status = await proc.wait();
-    logStatus(status);
+args = [exe, '-e', 'console.log(1+1)'];
+proc = tjs.spawn(args);
+console.log(`proc PID: ${proc.pid}`);
+status = await proc.wait();
+logStatus(status);
 
-    args = ['curl', '-s', '-i', 'https://bellard.org/quickjs/'];
-    proc = tjs.spawn(args);
-    console.log(`proc PID: ${proc.pid}`);
-    status = await proc.wait();
-    logStatus(status);
+args = ['curl', '-s', '-i', 'https://bellard.org/quickjs/'];
+proc = tjs.spawn(args);
+console.log(`proc PID: ${proc.pid}`);
+status = await proc.wait();
+logStatus(status);
 
-    proc = tjs.spawn('cat');
-    console.log(`proc PID: ${proc.pid}`);
-    proc.kill(tjs.SIGTERM);
-    status = await proc.wait();
-    logStatus(status);
-    status = await proc.wait();
-    logStatus(status);
+proc = tjs.spawn('cat');
+console.log(`proc PID: ${proc.pid}`);
+proc.kill(tjs.SIGTERM);
+status = await proc.wait();
+logStatus(status);
+status = await proc.wait();
+logStatus(status);
 
-    args = [exe, '-e', 'console.log(JSON.stringify(tjs.environ))'];
-    proc = tjs.spawn(args, { env: { FOO: 'BAR', SPAM: 'EGGS'} });
-    console.log(`proc PID: ${proc.pid}`);
-    status = await proc.wait();
-    logStatus(status);
-    
-    proc = tjs.spawn('cat', { stdin: 'pipe', stdout: 'pipe' });
-    console.log(`proc PID: ${proc.pid}`);
-    console.log(proc.stdin.fileno());
-    console.log(proc.stdout.fileno());
-    input = encoder.encode('hello!');
-    proc.stdin.write(input);
-    data = new Uint8Array(input.length);
-    await proc.stdout.read(data);
-    console.log(decoder.decode(data));
-    input = encoder.encode('hello again!');
-    proc.stdin.write(input);
-    data = new Uint8Array(input.length);
-    await proc.stdout.read(data);
-    console.log(decoder.decode(data));
-    proc.kill(tjs.SIGTERM);
-    status = await proc.wait();
-    console.log(status);
+args = [exe, '-e', 'console.log(JSON.stringify(tjs.environ))'];
+proc = tjs.spawn(args, { env: { FOO: 'BAR', SPAM: 'EGGS'} });
+console.log(`proc PID: ${proc.pid}`);
+status = await proc.wait();
+logStatus(status);
 
-})().catch(e => {
-    console.log(e);
-    console.log(e.stack);
-});
+proc = tjs.spawn('cat', { stdin: 'pipe', stdout: 'pipe' });
+console.log(`proc PID: ${proc.pid}`);
+console.log(proc.stdin.fileno());
+console.log(proc.stdout.fileno());
+input = encoder.encode('hello!');
+proc.stdin.write(input);
+data = new Uint8Array(input.length);
+await proc.stdout.read(data);
+console.log(decoder.decode(data));
+input = encoder.encode('hello again!');
+proc.stdin.write(input);
+data = new Uint8Array(input.length);
+await proc.stdout.read(data);
+console.log(decoder.decode(data));
+proc.kill(tjs.SIGTERM);
+status = await proc.wait();
+console.log(status);
