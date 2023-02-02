abacaa401ff7a7ff6d121c15f6de6223afcf58f2
Saúl Ibarra Corretgé <s@saghul.net>
2023-02-02 09:26:04 +0100

    test: make mem-limit test more resilient

--- tests/test-mem-limit.js
+++ tests/test-mem-limit.js
@@ -1,18 +1,19 @@
 import assert from 'tjs:assert';
 
+const MB = 1024 * 1024;
 
 const args = [
     tjs.exepath,
     '--memory-limit',
-    '10485760',
+    `${5 * MB}`,
     'eval',
-    'new Uint8Array(104857600)'
+    `const arr = new Uint8Array(${10 * MB}).fill(1); console.log(arr);`
 ];
 const proc = tjs.spawn(args, { stdout: 'ignore', stderr: 'pipe' });
 const buf = new Uint8Array(4096);
+const status = await proc.wait();
 const nread = await proc.stderr.read(buf);
 const stderrStr = new TextDecoder().decode(buf.subarray(0, nread));
-const status = await proc.wait();
 console.log(stderrStr);
 assert.ok(stderrStr.match(/InternalError: out of memory/) !== null, 'gives memory error');
 assert.ok(status.exit_status !== 0 && status.term_signal === null, 'script fails')
