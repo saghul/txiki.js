d05d49bad30894a00b208f793c2422835078a8a0
Saúl Ibarra Corretgé <s@saghul.net>
2023-09-26 21:52:26 +0200

    curl: fix WS crash

--- src/curl-utils.c
+++ src/curl-utils.c
@@ -117,7 +117,6 @@ int tjs_curl_load_http(DynBuf *dbuf, const char *url) {
 }
 
 static void check_multi_info(TJSRuntime *qrt) {
-    char *done_url;
     CURLMsg *message;
     int pending;
 
@@ -135,6 +134,12 @@ static void check_multi_info(TJSRuntime *qrt) {
                 tjs_curl_private_t *curl_private = NULL;
                 curl_easy_getinfo(easy_handle, CURLINFO_PRIVATE, &curl_private);
                 CHECK_NOT_NULL(curl_private);
+                /**
+                 * This is an ugly workaround. The WS code uses a _different_ private
+                 * struct and we need to tell them apart.
+                 */
+                if (curl_private->magic != TJS__CURL_PRIVATE_MAGIC)
+                    break;
                 CHECK_NOT_NULL(curl_private->done_cb);
                 curl_private->done_cb(message, curl_private->arg);
 
