08ee0c6e9511b33d0a4223acc515ab8062dd3d96
Saúl Ibarra Corretgé <s@saghul.net>
2023-10-03 23:07:51 +0200

    modules: add import.meta.path

--- tests/test-fs-stat.js
+++ tests/test-fs-stat.js
@@ -1,8 +1,7 @@
 import assert from 'tjs:assert';
-import path from 'tjs:path';
 
 
-const st = await tjs.stat(path.join(import.meta.dirname, import.meta.basename));
+const st = await tjs.stat(import.meta.path);
 
 assert.ok(st);
 assert.ok(st.isFile, 'is a regular file');
