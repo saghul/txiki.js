e6de3e4db5aefe47cf4155c81d0f4ea6e01373ca
Saúl Ibarra Corretgé <s@saghul.net>
2024-03-07 08:05:31 +0100

    polyfills: update URL polyfill
    Replace core-js with whatwg-url.

--- src/js/polyfills/index.js
+++ src/js/polyfills/index.js
@@ -3,20 +3,20 @@
 import './global.js';
 import './timers.js';
 import './dom-exception.js';
+import './event-target-polyfill.js';
 
 import './abba.js';
+import './text-encoding.js';
+import './text-encode-transform.js';
+import './url.js';
+
 import './navigator.js';
-import './event-target-polyfill.js';
-import './url-polyfill.js';
 
 import './xhr.js';
 import 'whatwg-fetch';
 import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
 
 import 'web-streams-polyfill/es2018';
-
-import './text-encoding.js';
-import './text-encode-transform.js';
 
 import './blob.js';
 import './file.js';
--- src/js/polyfills/url-polyfill.js
+++
@@ -1,7 +0,0 @@
-import 'core-js/actual/url';
-import 'core-js/actual/url/to-json';
-import 'core-js/actual/url-search-params';
-
-import { URLPattern } from 'urlpattern-polyfill';
-
-window.URLPattern = URLPattern;
