59dd7da74752aee1b6176978fb26cc042723b560
karurochari <nope>
2024-05-19 07:59:17 +0000

    Even more code

--- extras-helper.mjs
+++ extras-helper.mjs
@@ -1,10 +1,25 @@
 #!/bin/env node
-import { existsSync } from 'node:fs'
-import { readFile, writeFile, mkdir, rm } from 'node:fs/promises'
+import { existsSync, write } from 'node:fs'
+import { readFile, writeFile, mkdir, rm, cp } from 'node:fs/promises'
 
 import { program } from 'commander';
 import { randomUUID } from 'node:crypto';
 
+async function install(path) {
+    //TODO
+    //replace [module] && __module__
+}
+
+async function clear() {
+    await rm('extras/', { recursive: true, force: true });
+    await rm('src/extras/', { recursive: true, force: true });
+    await rm('src/js/extras/', { recursive: true, force: true });
+    await rm('tests/extras/', { recursive: true, force: true });
+    await rm('examples/extras/', { recursive: true, force: true });
+    await rm('deps/extras/', { recursive: true, force: true });
+    await rm('benchmark/extras/', { recursive: true, force: true });
+    await rm('docs/types/extras/', { recursive: true, force: true });
+}
 
 program
     .name('extras-helper.mjs')
@@ -13,32 +28,24 @@ program
 program.command('clear')
     .description('Clear after your previous configuration')
     .action(async () => {
-        await rm('extras/', { recursive: true, force: true });
-        await rm('src/extras/', { recursive: true, force: true });
-        await rm('src/js/extras/', { recursive: true, force: true });
-        await rm('tests/extras/', { recursive: true, force: true });
-        await rm('examples/extras/', { recursive: true, force: true });
-        await rm('deps/extras/', { recursive: true, force: true });
-        await rm('benchmark/extras/', { recursive: true, force: true });
-        await rm('docs/types/extras/', { recursive: true, force: true });
-
+        await clear()
     })
 
 program.command('clone')
     .description('Clear after your previous configuration')
     .argument("[filename]", 'filename for the configuration', './modules.json')
     .action(async (filename) => {
-        if (!existsSync("extras/")) await mkdir("extras/");
-        if (!existsSync("src/extras/")) await mkdir('src/extras/');
-        if (!existsSync("src/js/extras/")) await mkdir('src/js/extras/');
-        if (!existsSync("tests/extras/")) await mkdir('tests/extras/');
-        if (!existsSync("examples/extras/")) await mkdir('examples/extras/');
-        if (!existsSync("deps/extras/")) await mkdir('deps/extras/');
-        if (!existsSync("benchmark/extras/")) await mkdir('benchmark/extras/');
-        if (!existsSync("docs/types/extras/")) await mkdir('docs/types/extras/');
+        //For now, since I am too lazy to handle merging
+        await clear()
 
-        //Placeholder for now
-        await writeFile('deps/extras/CMakeLists.txt', '')
+        await mkdir("extras/", { errorOnExist: false });
+        await mkdir('src/extras/', { errorOnExist: false });
+        await mkdir('src/js/extras/', { errorOnExist: false });
+        await mkdir('tests/extras/', { errorOnExist: false });
+        await mkdir('examples/extras/', { errorOnExist: false });
+        await mkdir('deps/extras/', { errorOnExist: false });
+        await mkdir('benchmark/extras/', { errorOnExist: false });
+        await mkdir('docs/types/extras/', { errorOnExist: false });
 
         let config = undefined
         try {
@@ -49,21 +56,63 @@ program.command('clone')
             process.exit(1)
         }
 
-        for (const file of Object.entries(config)) {
-            console.log(file)
+        for (const module of Object.entries(config)) {
+            //From the internet
+            if (module[1].startsWith('https://') || module[1].startsWith('http://')) {
+                //TODO: Copy from the internet & unpack
+                //Need to check for deps
+            }
+            //Local folder
+            else {
+                //TODO: Copy from local fs
+                await cp(module[1], `./extras/${module[0]}`, { recursive: true, dereference: true, errorOnExist: false })
+            }
+            await install(`./extras/${module[0]}`)
         }
 
+        //Placeholder for now
+        await writeFile('deps/extras/CMakeLists.txt', '')
+        await writeFile('./modules.json', JSON.stringify(config, null, 4))
+
+        //Construct src/extras.bootstrap
+        await writeFile('./src/extras.bootstrap', Object.keys(config).map(x => `tjs__mod_${x}_init(ctx, ns);`).join('\n'))
     })
 
 program.command('extract')
     .description('Utility command to extract a module from this repo')
     .argument("<module>", "the module name to select from")
-    .argument("[where]", 'the destination folder for the files to be copied over', `/tmp/${randomUUID()}`)
-    .action(async (where) => {
+    .argument("[where]", 'the destination folder for the files to be copied over')
+    .action(async (module, where) => {
         //TODO: If not found list the ones available
 
-        //TODO: Fold back
-        console.log(`Files copied over to ${where}`)
+        let config = undefined
+        try {
+            config = JSON.parse(await readFile('./modules.json'))
+        }
+        catch (e) {
+            console.error("Unable to parse the config file.")
+            process.exit(1)
+        }
+
+        if (Object.keys(config).includes(module)) {
+            const location = config[module]
+            //Remote source
+            if (location.startsWith('https://') || location.startsWith('http://')) {
+                where ??= `/tmp/${randomUUID()}`
+                console.error('Packing for remote modules not yet supported')
+            }
+            //Local folder
+            else {
+                where ??= location
+                //TODO: Fold back
+            }
+            console.log(`Files copied over to ${where}`)
+        }
+
+        else {
+            console.error('Name not matched, select from', Object.keys(config))
+        }
+
     })
 
 program.parse();
