09af900ecce8c8a7471dfa080160a346b67964f3
Saúl Ibarra Corretgé <s@saghul.net>
2024-06-06 10:14:46 +0200

    sqlite: add transactions support
    Fixes: https://github.com/saghul/txiki.js/issues/511

--- src/js/stdlib/sqlite.js
+++ src/js/stdlib/sqlite.js
@@ -2,6 +2,7 @@ const core = globalThis[Symbol.for('tjs.internal.core')];
 const sqlite3 = core._sqlite3;
 
 const kSqlite3Handle = Symbol('kSqlite3Handle');
+let controllers;
 
 class Database {
     constructor(dbName = ':memory:', options = { create: true, readOnly: false }) {
@@ -23,6 +24,7 @@ class Database {
     close() {
         if (this[kSqlite3Handle]) {
             sqlite3.close(this[kSqlite3Handle]);
+            this[kSqlite3Handle] = null;
         }
     }
 
@@ -41,7 +43,106 @@ class Database {
 
         return new Statement(sqlite3.prepare(this[kSqlite3Handle], sql));
     }
+
+    // Code for transactions is largely copied from better-sqlite3 and Bun
+    // https://github.com/JoshuaWise/better-sqlite3/blob/master/lib/methods/transaction.js
+    // https://github.com/oven-sh/bun/blob/main/src/js/bun/sqlite.ts
+
+    get inTransaction() {
+        if (!this[kSqlite3Handle]) {
+            return false;
+        }
+
+        return sqlite3.in_transaction(this[kSqlite3Handle]);
+    }
+
+    transaction(fn) {
+        if (typeof fn !== 'function') {
+            throw new TypeError('Expected first argument to be a function');
+        }
+
+        const db = this;
+        const controller = getController(db);
+
+        // Each version of the transaction function has these same properties.
+        const properties = {
+            default: { value: wrapTransaction(fn, db, controller.default) },
+            deferred: { value: wrapTransaction(fn, db, controller.deferred) },
+            immediate: { value: wrapTransaction(fn, db, controller.immediate) },
+            exclusive: { value: wrapTransaction(fn, db, controller.exclusive) },
+        };
+
+        Object.defineProperties(properties.default.value, properties);
+        Object.defineProperties(properties.deferred.value, properties);
+        Object.defineProperties(properties.immediate.value, properties);
+        Object.defineProperties(properties.exclusive.value, properties);
+
+        // Return the default version of the transaction function.
+        return properties.default.value;
+    }
 }
+
+// Return the database's cached transaction controller, or create a new one.
+const getController = db => {
+    let controller = (controllers ||= new WeakMap()).get(db);
+
+    if (!controller) {
+        const shared = {
+            commit: db.prepare('COMMIT'),
+            rollback: db.prepare('ROLLBACK'),
+            savepoint: db.prepare('SAVEPOINT `\t_bs3.\t`'),
+            release: db.prepare('RELEASE `\t_bs3.\t`'),
+            rollbackTo: db.prepare('ROLLBACK TO `\t_bs3.\t`'),
+        };
+
+        controller = {
+            default: Object.assign({ begin: db.prepare('BEGIN') }, shared),
+            deferred: Object.assign({ begin: db.prepare('BEGIN DEFERRED') }, shared),
+            immediate: Object.assign({ begin: db.prepare('BEGIN IMMEDIATE') }, shared),
+            exclusive: Object.assign({ begin: db.prepare('BEGIN EXCLUSIVE') }, shared),
+        };
+
+        controllers.set(db, controller);
+    }
+
+    return controller;
+};
+
+// Return a new transaction function by wrapping the given function.
+const wrapTransaction = (fn, db, { begin, commit, rollback, savepoint, release, rollbackTo }) =>
+    function transaction() {
+        let before, after, undo;
+
+        if (db.inTransaction) {
+            before = savepoint;
+            after = release;
+            undo = rollbackTo;
+        } else {
+            before = begin;
+            after = commit;
+            undo = rollback;
+        }
+
+        try {
+            before.run();
+
+            const result = Function.prototype.apply.call(fn, this, arguments);
+
+            after.run();
+
+            return result;
+        } catch (ex) {
+            if (db.inTransaction) {
+                undo.run();
+
+                if (undo !== rollback) {
+                    after.run();
+                }
+            }
+
+            throw ex;
+        }
+    };
 
 const kSqlite3Stmt = Symbol('kSqlite3Stmt');
 
--- tests/test-sqlite.js
+++ tests/test-sqlite.js
@@ -82,3 +82,116 @@ assert.ok(result.isFile, 'file was created ok');
 await tjs.unlink(newDb);
 
 testNewDbNoCreate();
+
+function testTransactions() {
+    const db = new Database();
+
+    assert.falsy(db.inTransaction);
+
+    db.exec('CREATE TABLE test (txt TEXT NOT NULL, int INTEGER, double FLOAT, data BLOB)');
+
+    const ins = db.prepare('INSERT INTO test (txt, int, double, data) VALUES(?, ?, ?, ?)');
+    const insMany = db.transaction(datas => {
+        assert.ok(db.inTransaction);
+
+        for (const data of datas) {
+            ins.run(data);
+        }
+    });
+
+    insMany([
+        [ 'foo', 42, 4.2, new Uint8Array(16).fill(42) ],
+        [ 'foo', 43, 4.3, new Uint8Array(16).fill(43) ],
+        [ 'bar', 69, 6.9, new Uint8Array(16).fill(69) ],
+        [ 'baz', 666, 6.6, null ],
+    ]);
+
+    const data1 = db.prepare('SELECT * FROM test').all();
+
+    assert.eq(data1.length, 4);
+}
+
+function testTransactionsError() {
+    const db = new Database();
+
+    assert.falsy(db.inTransaction);
+
+    db.exec('CREATE TABLE test (txt TEXT NOT NULL, int INTEGER, double FLOAT, data BLOB)');
+
+    const ins = db.prepare('INSERT INTO test (txt, int, double, data) VALUES(?, ?, ?, ?)');
+    const insMany = db.transaction(datas => {
+        assert.ok(db.inTransaction);
+
+        for (const data of datas) {
+            ins.run(data);
+        }
+
+        throw new Error('oops!');
+    });
+
+    assert.throws(() => insMany([
+        [ 'foo', 42, 4.2, new Uint8Array(16).fill(42) ],
+        [ 'foo', 43, 4.3, new Uint8Array(16).fill(43) ],
+        [ 'bar', 69, 6.9, new Uint8Array(16).fill(69) ],
+        [ 'baz', 666, 6.6, null ],
+    ]), Error, 'an error is thrown');
+
+    const data1 = db.prepare('SELECT * FROM test').all();
+
+    assert.falsy(db.inTransaction);
+    assert.eq(data1.length, 0);
+}
+
+function testTransactionsNested() {
+    const db = new Database();
+
+    assert.falsy(db.inTransaction);
+
+    db.exec('CREATE TABLE test (txt TEXT NOT NULL, int INTEGER, double FLOAT, data BLOB)');
+
+    const ins = db.prepare('INSERT INTO test (txt, int, double, data) VALUES(?, ?, ?, ?)');
+    const ins2 = db.prepare('INSERT INTO test (txt, int) VALUES(?, ?)');
+
+    const insMany = db.transaction(datas => {
+        assert.ok(db.inTransaction);
+
+        for (const data of datas) {
+            ins.run(data);
+        }
+
+        throw new Error('oops!');
+    });
+
+    const insMany2 = db.transaction(datas => {
+        assert.ok(db.inTransaction);
+
+        for (const data of datas) {
+            ins.run(data);
+        }
+
+        try {
+            insMany([
+                [ 'foo', 42, 4.2, new Uint8Array(16).fill(42) ],
+                [ 'foo', 43, 4.3, new Uint8Array(16).fill(43) ],
+                [ 'bar', 69, 6.9, new Uint8Array(16).fill(69) ],
+                [ 'baz', 666, 6.6, null ]
+            ]);
+        } catch(_) {
+            // Ignore, so the outer transaction succeeds.
+        }
+    });
+
+    insMany2([
+        [ '1234', 1234 ],
+        [ '4321', 4321 ],
+    ]);
+
+    const data1 = db.prepare('SELECT * FROM test').all();
+
+    assert.falsy(db.inTransaction);
+    assert.eq(data1.length, 2);
+}
+
+testTransactions();
+testTransactionsError();
+testTransactionsNested()
