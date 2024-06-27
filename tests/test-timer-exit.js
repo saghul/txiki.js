fcf87718a9be31353d1db0b7216291d917b371e8
Saúl Ibarra Corretgé <s@saghul.net>
2024-06-27 23:39:06 +0200

    test: add test for runtime exit with an exception on a timer

---
+++ tests/test-timer-exit.js
@@ -0,0 +1,16 @@
+import assert from 'tjs:assert';
+import path from 'tjs:path';
+
+
+const args = [
+    tjs.exepath,
+    'run',
+    path.join(import.meta.dirname, 'helpers', 'timers2.js')
+];
+const proc = tjs.spawn(args, { stdout: 'ignore', stderr: 'pipe' });
+const buf = new Uint8Array(4096);
+const nread = await proc.stderr.read(buf);
+const stderrStr = new TextDecoder().decode(buf.subarray(0, nread));
+const status = await proc.wait();
+assert.ok(stderrStr.match(/Error: oops!/) !== null, 'dumps to stderr');
+assert.ok(status.exit_status !== 0 && status.term_signal === null, 'succeeded');
