305a5b3542cce10df1c0474b92044a877b656ab3
Saúl Ibarra Corretgé <s@saghul.net>
2024-06-27 23:57:34 +0200

    test: fix xhr test
    The previous commit uncovered this bug!

--- tests/test-xhr-body.js
+++ tests/test-xhr-body.js
@@ -11,6 +11,6 @@ xhr.onloadend = () => {
     assert.eq(xhr.readyState, xhr.DONE, 'readyState is DONE');
     assert.eq(xhr.responseURL, url, 'url is the same');
     assert.eq(xhr.status, 200, 'status is 200');
-    assert.eq(xhr.response.data, data, 'sent and received data match');
+    assert.eq(JSON.stringify(xhr.response.data), data, 'sent and received data match');
 };
 xhr.send(data);
