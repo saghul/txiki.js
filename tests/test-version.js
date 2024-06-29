4a59e888e20e4018a9f49b2d94348232c87f6404
karurochari <nope>
2024-06-29 23:12:34 +0000

    Add sqlite3 version test

--- tests/test-version.js
+++ tests/test-version.js
@@ -5,3 +5,4 @@ assert.ok(tjs.version, 'tjs.version is defined');
 assert.ok(tjs.versions, 'tjs.versions is defined');
 assert.ok(tjs.versions.tjs, 'tjs.versions.tjs is defined');
 assert.ok(tjs.versions.uv, 'tjs.versions.uv is defined');
+assert.ok(tjs.versions.sqlite3, 'tjs.versions.sqlite3 is defined');
