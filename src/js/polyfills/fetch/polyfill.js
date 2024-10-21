25a2f3dcbbc8f1e40de63127278d4afce16cac4b
Eros Zhao <zyeros1991@gmail.com>
2024-10-21 16:00:13 +0000

    style: remove unnecessary whitespace at polyfill.js

--- src/js/polyfills/fetch/polyfill.js
+++ src/js/polyfills/fetch/polyfill.js
@@ -3,7 +3,6 @@ import { Headers } from './headers.js';
 import { Request } from './request.js';
 import { Response } from './response.js';
 
-
 globalThis.fetch = fetch;
 globalThis.Headers = Headers;
 globalThis.Request = Request;
