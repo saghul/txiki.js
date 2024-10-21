8446a3b65e55eaacd9ae8e4c6a6b6c92733132fa
Eros Zhao <zyeros1991@gmail.com>
2024-10-21 14:22:14 +0000

    feat: export Headers class object to globalThis

--- src/js/polyfills/fetch/polyfill.js
+++ src/js/polyfills/fetch/polyfill.js
@@ -1,8 +1,10 @@
 import { fetch } from './fetch.js';
 import { Request } from './request.js';
 import { Response } from './response.js';
+import { Headers } from './headers.js';
 
 
 globalThis.fetch = fetch;
 globalThis.Request = Request;
 globalThis.Response = Response;
+globalThis.Headers = Headers;
