ccbb79cb941fcaea0283577b22deedf27fddbfa4
Saúl Ibarra Corretgé <s@saghul.net>
2026-02-09 23:04:47 +0100

    process: consolidate code for subprocess stdio handles

--- src/js/core/process.js
+++ src/js/core/process.js
@@ -1,7 +1,3 @@
-import {
-    initWriteQueue, writeWithQueue, readableStreamForHandle, writableStreamForHandle
-} from './stream-utils.js';
-
 const core = globalThis[Symbol.for('tjs.internal.core')];
 
 const kProcess = Symbol('kProcess');
@@ -9,36 +5,11 @@ const kStdin = Symbol('kStdin');
 const kStdout = Symbol('kStdout');
 const kStderr = Symbol('kStderr');
 
-class Subprocess {
-    constructor(proc, stdin, stdout, stderr) {
-        this[kProcess] = proc;
-        this[kStdin] = stdin ?? null;
-        this[kStdout] = stdout ?? null;
-        this[kStderr] = stderr ?? null;
-    }
-
-    get pid() {
-        return this[kProcess].pid;
-    }
-
-    get stdin() {
-        return this[kStdin];
-    }
-
-    get stdout() {
-        return this[kStdout];
-    }
-
-    get stderr() {
-        return this[kStderr];
-    }
-
-    kill(sig) {
-        return this[kProcess].kill(sig);
-    }
-
-    wait() {
-        return this[kProcess].wait();
+function silentClose(handle) {
+    try {
+        handle.close();
+    } catch {
+        // Ignored.
     }
 }
 
@@ -74,32 +45,139 @@ async function readAllChunks(stream) {
     return result;
 }
 
-function addStreamHelpers(stream) {
-    Object.defineProperty(stream, 'arrayBuffer', {
-        value() {
-            return readAllChunks(this).then(bytes => bytes.buffer);
-        }
-    });
+class ProcessReadableStream extends ReadableStream {
+    constructor(handle) {
+        let reading = false;
 
-    Object.defineProperty(stream, 'bytes', {
-        value() {
-            return readAllChunks(this);
-        }
-    });
+        super({
+            start(controller) {
+                handle.onread = chunk => {
+                    if (chunk instanceof Uint8Array) {
+                        controller.enqueue(chunk);
 
-    Object.defineProperty(stream, 'text', {
-        value() {
-            return readAllChunks(this).then(bytes => new TextDecoder().decode(bytes));
-        }
-    });
+                        if (controller.desiredSize <= 0) {
+                            handle.stopRead();
+                            reading = false;
+                        }
+                    } else if (chunk === null) {
+                        reading = false;
+                        controller.close();
+                        silentClose(handle);
+                    } else if (chunk instanceof Error) {
+                        reading = false;
+                        controller.error(chunk);
+                        silentClose(handle);
+                    } else {
+                        // undefined - handle closed
+                        reading = false;
+                        controller.close();
+                    }
+                };
+            },
+            pull() {
+                if (!reading) {
+                    reading = true;
+                    handle.startRead();
+                }
+            },
+            cancel() {
+                if (reading) {
+                    handle.stopRead();
+                    reading = false;
+                }
 
-    return stream;
+                handle.onread = null;
+                silentClose(handle);
+            }
+        });
+    }
+
+    arrayBuffer() {
+        return readAllChunks(this).then(bytes => bytes.buffer);
+    }
+
+    bytes() {
+        return readAllChunks(this);
+    }
+
+    text() {
+        return readAllChunks(this).then(bytes => new TextDecoder().decode(bytes));
+    }
 }
 
-function createWritableForPipe(handle) {
-    const writeQueue = initWriteQueue(handle);
+class ProcessWritableStream extends WritableStream {
+    constructor(handle) {
+        const queue = [];
 
-    return writableStreamForHandle(handle, buf => writeWithQueue(handle, writeQueue, buf));
+        handle.onwrite = error => {
+            const entry = queue.shift();
+
+            if (entry) {
+                if (error) {
+                    entry.reject(error);
+                } else {
+                    entry.resolve();
+                }
+            }
+        };
+
+        super({
+            async write(chunk, controller) {
+                try {
+                    const result = handle.write(chunk);
+
+                    if (typeof result !== 'number') {
+                        const { promise, resolve, reject } = Promise.withResolvers();
+
+                        queue.push({ resolve, reject });
+                        await promise;
+                    }
+                } catch (e) {
+                    controller.error(e);
+                    silentClose(handle);
+                }
+            },
+            close() {
+                silentClose(handle);
+            },
+            abort() {
+                silentClose(handle);
+            }
+        });
+    }
+}
+
+class Subprocess {
+    constructor(proc, stdin, stdout, stderr) {
+        this[kProcess] = proc;
+        this[kStdin] = stdin ?? null;
+        this[kStdout] = stdout ?? null;
+        this[kStderr] = stderr ?? null;
+    }
+
+    get pid() {
+        return this[kProcess].pid;
+    }
+
+    get stdin() {
+        return this[kStdin];
+    }
+
+    get stdout() {
+        return this[kStdout];
+    }
+
+    get stderr() {
+        return this[kStderr];
+    }
+
+    kill(sig) {
+        return this[kProcess].kill(sig);
+    }
+
+    wait() {
+        return this[kProcess].wait();
+    }
 }
 
 export function spawn(args, options) {
@@ -110,21 +188,21 @@ export function spawn(args, options) {
         const handle = new core.Pipe();
 
         opts.stdin = handle;
-        stdin = createWritableForPipe(handle);
+        stdin = new ProcessWritableStream(handle);
     }
 
     if (opts.stdout === 'pipe') {
         const handle = new core.Pipe();
 
         opts.stdout = handle;
-        stdout = addStreamHelpers(readableStreamForHandle(handle));
+        stdout = new ProcessReadableStream(handle);
     }
 
     if (opts.stderr === 'pipe') {
         const handle = new core.Pipe();
 
         opts.stderr = handle;
-        stderr = addStreamHelpers(readableStreamForHandle(handle));
+        stderr = new ProcessReadableStream(handle);
     }
 
     const proc = core.spawn(args, opts);
