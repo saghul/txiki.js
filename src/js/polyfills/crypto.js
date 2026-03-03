84c6efd6475cbc4aa3c8609ba9e9b087915188c2
Saúl Ibarra Corretgé <s@saghul.net>
2026-03-03 15:24:58 +0100

    webcrypto: add crypto.subtle.digest

--- src/js/polyfills/crypto.js
+++ src/js/polyfills/crypto.js
@@ -3,6 +3,15 @@ const core = globalThis[Symbol.for('tjs.internal.core')];
 const TypedArrayPrototype = Object.getPrototypeOf(Uint8Array.prototype);
 const TypedArrayProto_toStringTag = Object.getOwnPropertyDescriptor(TypedArrayPrototype, Symbol.toStringTag).get;
 
+const nativeDigest = core.webcrypto.digest;
+
+const digestAlgorithms = {
+    'SHA-1':   nativeDigest.DIGEST_SHA1,
+    'SHA-256': nativeDigest.DIGEST_SHA256,
+    'SHA-384': nativeDigest.DIGEST_SHA384,
+    'SHA-512': nativeDigest.DIGEST_SHA512,
+};
+
 
 function getRandomValues(obj) {
     const type = TypedArrayProto_toStringTag.call(obj);
@@ -35,11 +44,45 @@ function randomUUID() {
     return core.randomUUID();
 }
 
+class SubtleCrypto {
+    digest(algorithm, data) {
+        const name = typeof algorithm === 'string' ? algorithm : algorithm?.name;
+        const typeId = digestAlgorithms[name];
+
+        if (typeId === undefined) {
+            return Promise.reject(new DOMException(`Unrecognized algorithm name: ${name}`, 'NotSupportedError'));
+        }
+
+        let bytes;
+
+        if (ArrayBuffer.isView(data)) {
+            bytes = new Uint8Array(data.buffer, data.byteOffset, data.byteLength);
+        } else if (data instanceof ArrayBuffer) {
+            bytes = new Uint8Array(data);
+        } else {
+            return Promise.reject(new TypeError('data must be a BufferSource'));
+        }
+
+        const { promise, resolve, reject } = Promise.withResolvers();
+
+        nativeDigest(typeId, bytes, (err, result) => {
+            if (err) {
+                reject(new DOMException(err, 'OperationError'));
+            } else {
+                resolve(result.buffer);
+            }
+        });
+
+        return promise;
+    }
+}
+
+const subtle = new SubtleCrypto();
 
 const crypto = Object.freeze({
     getRandomValues,
     randomUUID,
-    subtle: undefined
+    subtle,
 });
 
 Object.defineProperty(window, 'crypto', {
@@ -48,4 +91,3 @@ Object.defineProperty(window, 'crypto', {
     writable: true,
     value: crypto
 });
-
