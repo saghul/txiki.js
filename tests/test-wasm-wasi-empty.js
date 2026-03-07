1124b80069dafa54a20b3a4e51a93e4d9c95382f
Saúl Ibarra Corretgé <s@saghul.net>
2026-03-08 00:33:15 +0100

    fs: avoid using the JS allocator in the worker thread
    It's not thread-safe.

--- tests/test-wasm-wasi-empty.js
+++ tests/test-wasm-wasi-empty.js
@@ -10,4 +10,4 @@ const proc = tjs.spawn(args, { stderr: 'pipe' });
 const [ status, dataStr ] = await Promise.all([ proc.wait(), proc.stderr.text() ]);
 assert.eq(status.exit_status, 1, 'WASI failed to run');
 assert.ok(dataStr.length > 0, 'stderr was read');
-assert.ok(dataStr.match(/TypeError: invalid buffer/));
+assert.ok(dataStr.match(/TypeError:.*invalid buffer/));
