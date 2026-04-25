a40e71c94eba3b3e2089df64750e32cf4439c8d5
Joey <fishjojo1@gmail.com>
2026-04-25 15:46:25 +0800

    timers: fix reentrant timer self-clear

--- tests/test-timer.js
+++ tests/test-timer.js
@@ -14,3 +14,27 @@ const runner2 = () => {
 };
 await runner2();
 assert.ok(true, 'setTimeout timer should be supported');
+
+const runner3 = () => {
+    return new Promise(resolve => {
+        const siblings = [];
+        for (let i = 0; i < 32; i++) {
+            siblings.push(setTimeout(() => {}, 1_000_000));
+        }
+
+        let selfId;
+        let fired = false;
+        selfId = setTimeout(() => {
+            clearTimeout(selfId);
+            fired = true;
+        }, 1);
+
+        setTimeout(() => {
+            for (const id of siblings) {
+                clearTimeout(id);
+            }
+            resolve(fired);
+        }, 50);
+    });
+};
+assert.ok(await runner3(), 'timer can clear itself from its callback');
